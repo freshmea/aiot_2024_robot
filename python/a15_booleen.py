ed30df705efb6d760f31aebec93145c4e127d05d
freshmea <freshmea@naver.com>
2024-09-19 14:52:50 +0900

    aa

--- python/a15_booleen.py
+++ python/a15_booleen.py
@@ -8,6 +8,13 @@ def main():
         print("bo1 is True")
     else:
         print("bo1 is False")
+        
+    # c (!true) -> false
+    # python (not True) -> False
+    # c ( ACondition && BCondition )
+    # python ( ACondition and BCondition )
+    # c ( ACondition || BCondition)
+    # python (ACondition or BCondition)
 
 if __name__ == "__main__":
     main()
