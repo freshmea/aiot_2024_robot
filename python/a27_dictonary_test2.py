322e112ce0bbb0bf7e933b4b390f328de98aa3cd
freshmea <freshmea@naver.com>
2024-09-20 11:53:49 +0900

    aa

--- python/a27_dictonary_test2.py
+++ python/a27_dictonary_test2.py
@@ -5,20 +5,28 @@ def main():
         "ingredient": ["망고", "설탕", "메타중아황산나트륨", "치자황색소"],
         "origin": "필리핀",
     }
-    print(f'name: {dictionary["name"]}')
-    print(f'type: {dictionary["type"]}')
-    print(f'ingredient: {dictionary["ingredient"]}')
-    print(f'origin: {dictionary["origin"]}')
+    # print(f'name: {dictionary["name"]}')
+    # print(f'type: {dictionary["type"]}')
+    # print(f'ingredient: {dictionary["ingredient"]}')
+    # print(f'origin: {dictionary["origin"]}')
+    
+    # for key in dictionary:
+    #     print(f"{key} : {dictionary[key]}")
+    
+    for key, value in dictionary.items():
+        print(f"{key} : {value}")
 
     dictionary["name"] = "8D 건조 망고"
     print(f'name: {dictionary["name"]}')
     print(f"ingredient[1]: {dictionary['ingredient'][1]}")
 
     key = input("키를 입력해 주세요>")
-    if key in dictionary:
-        print(f"value : {dictionary[key]}")
-    else:
-        print("그 키는 dictionary 에 없습니다.")
+    # if key in dictionary:
+    #     print(f"value : {dictionary[key]}")
+    # else:
+    #     print("그 키는 dictionary 에 없습니다.")
+    value = dictionary.get(key)
+    print(f"value : {value}")
 
     #제거
     del dictionary["ingredient"]
---
+++ python/a28_dictionary_test3.py
@@ -0,0 +1,23 @@
+def main():
+    character = {
+        "name": "기사",
+        "level": 12,
+        "items": {
+            "sword": "불꽃의 검",
+            "armor": "풀플레이트",
+        },
+        "skill": ["베기", "세게 베기", "아주 세게 베기"]
+    }
+
+    for key, value in character.items():
+        if isinstance(character[key], dict):
+            for key in value:
+                print(f"{key} : {value[key]}")
+        elif isinstance(character[key], list):
+            for ele in value:
+                print(f"{key} : {ele}")
+        else:
+            print(f"{key} : {value}")
+
+if __name__ == "__main__":
+    main()
