6a96c1e85575f353b0356a9f11e9dc7d47941bec
freshmea <freshmea@naver.com>
2024-09-20 15:23:05 +0900

    exception

--- python/a33_function_test2.py
+++ python/a33_function_test2.py
@@ -15,15 +15,19 @@ def abc_print(a, b = "요", c = ".", *d):
     print(f"a: {a}")
     print(f"b: {b}")
     print(f"c: {c}")
+    return a + b + c, b + c + a
 
 def main():
     print_n_times("안녕하세요.", "파이썬", "방갑습니다.", n=3)
     # abc_print( "안녕", c ="하세")
     abc_print( "안녕", "하세", "입니다.", "a", "b")
-    
+
     # 가변 키워드 매개변수
     print_n_times("안녕하세요.", "파이썬", "방갑습니다.", n=3, m = 4, l = 2)
-    
+
+    re1, re2 = abc_print("a", "b", "c")
+    print(type(re1))
+    print(re1, re2)
 
 if __name__ == "__main__":
     main()
