fd884ea05f2534b44151115f552cbf7e54615bdc
freshmea <freshmea@naver.com>
2024-09-23 10:56:25 +0900

    special method 2

--- python/a41_class_special_method.py
+++ python/a41_class_special_method.py
@@ -5,22 +5,38 @@ class Student:
         self.math =  math
         self.english = english
         self.science =  science
+        self.sum = self.get_sum()
+        self.average = self.get_average()
 
     def get_sum(self):
         return self.korean + self.math + self.english + self.science
 
     def get_average(self):
-        self.sum = self.get_sum()
         return self.sum / 4
 
     def to_string(self):
         return f"{self.name}\t{self.get_sum()}\t{self.get_average():.2f}"
 
-    # def __repr__(self):
-    #     return f"{self.name}\t{self.get_sum()}\t{self.get_average():.2f}"
-
     def __str__(self):
         return f"{self.name}\t{self.get_sum()}\t{self.get_average():.2f}"
+
+    def __eq__(self, value):
+        return self.sum  == value.sum
+
+    def __ne__(self, value):
+        return self.sum  != value.sum
+
+    def __gt__(self, value):
+        return self.sum  > value.sum
+
+    def __ge__(self, value):
+        return self.sum  >= value.sum
+
+    def __lt__(self, value):
+        return self.sum  < value.sum
+
+    def __le__(self, value):
+        return self.sum  <= value.sum
 
 def main():
     students = [
@@ -36,6 +52,12 @@ def main():
     for student in students:
         print(student.to_string())
         print(student)
+
+    student_a = Student("윤인성", 87, 98, 88, 95)
+    student_b = Student("연하진", 84, 98, 96, 90)
+    print(f"student_a == student_b : {student_a == student_b}")
+    print(f"student_a is student_b : {student_a is student_b}")
+    print(f"student_a != student_b : {student_a != student_b}")
 
 if __name__ == "__main__":
     main()
