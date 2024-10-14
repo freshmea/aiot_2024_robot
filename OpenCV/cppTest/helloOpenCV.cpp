fa05c7c197df7bd6dbb07c3498a3fc6c9a322a81
freshmea <freshmea@naver.com>
2024-10-14 11:35:36 +0900

    imwrite empty

--- OpenCV/cppTest/helloOpenCV.cpp
+++ OpenCV/cppTest/helloOpenCV.cpp
@@ -1,12 +1,24 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <string>
 
 int main()
 {
     cv::Mat img;
+    std::cout << img.empty() << std::endl;
     img = cv::imread("lena.bmp");
+    if (img.empty())
+    {
+        std::cout << "Image is empty" << std::endl;
+        return -1;
+    }
+
+    std::string windowName = "image";
+    cv::String windowName2 = "image2";
+
     cv::imshow("image", img);
     std::cout << "Hello OpenCV" << std::endl;
+    cv::imwrite("lena.jpg", img);
     cv::waitKey(0);
     return 0;
 }
