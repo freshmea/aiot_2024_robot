440ba21c69857dfdb3d085259b7dc100bffd888e
freshmea <freshmea@naver.com>
2024-10-15 10:38:24 +0900

    aa

--- OpenCV/cppTest/mouse.cpp
+++ OpenCV/cppTest/mouse.cpp
@@ -21,12 +21,18 @@ int main()
 void onMouse(int event, int x, int y, int flags, void *data)
 {
     Mat *src = (Mat *)data;
+    Mat temp;
+    temp = (*src).clone();
     switch (event)
     {
     case EVENT_LBUTTONDOWN:
         cout << x << y << endl;
         circle(*src, Point(x, y), 10, Scalar(255, 0, 0), -1);
         imshow("img", *src);
+        break;
+    case EVENT_MOUSEMOVE:
+        rectangle(temp, Rect(x, y, 50, 50), Scalar(0, 0, 255), 5);
+        imshow("img", temp);
         break;
     }
 }
