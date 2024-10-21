cf7e381cc262150a42f147bd875729dfb8322aea
freshmea <freshmea@naver.com>
2024-10-21 12:54:21 +0900

    aa

--- aiot_ws/src/simple_ros/setup.py
+++ aiot_ws/src/simple_ros/setup.py
@@ -13,7 +13,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.lanuch.py')))
+        ('share/' + package_name + '/launch', glob(os.path.join('launch', '*.launch.py')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
