12cfd184ea565614d614c609a8801a21d9e92fbe
freshmea <freshmea@naver.com>
2024-10-23 12:49:35 +0900

    aa

--- aiot_ws/src/simple_ros/launch/two_turtle.launch.py
+++ aiot_ws/src/simple_ros/launch/two_turtle.launch.py
@@ -2,7 +2,7 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -12,17 +12,20 @@ def generate_launch_description():
                                     default=os.path.join(
                                         get_package_share_directory('simple_ros'),
                                         'param',
-                                        'simple_parameter.yaml'))
+                                        'turtlesim.yaml'))
     return LaunchDescription([
         DeclareLaunchArgument('param_dir',
                               default_value=param_dir,
                               description="simple parameter"),
         Node(package="turtlesim",
              executable="turtlesim_node",
-             parameters=[param_dir],
-             namespace="screen1"),
+             parameters=[param_dir]
+             ),
+        ExecuteProcess(cmd=[['ros2 service call ','turtle1', '/spawn ', 'turtlesim/srv/Spawn ', '"{x: 2, y: 2, theta: 0.2}"' ]]),
         Node(package="simple_ros",
-             executable="simple_parameter",
-             parameters=[param_dir],
-             namespace="screen2"),
+             executable="move_turtle",
+             parameters=[param_dir]
+             ),
+        
+
         ])
