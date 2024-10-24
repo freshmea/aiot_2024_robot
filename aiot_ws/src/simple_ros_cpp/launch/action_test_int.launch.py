23fd93fe06df833e727bd7d5557047541ae7b838
freshmea <freshmea@naver.com>
2024-10-24 12:50:23 +0900

    launch

--- aiot_ws/src/simple_ros_cpp/launch/action_test_int.launch.py
+++ aiot_ws/src/simple_ros_cpp/launch/action_test_int.launch.py
@@ -5,5 +5,5 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription([
         Node(package="simple_ros_cpp", executable="action_server"),
-        Node(package="simple_ros_cpp", executable="action_client")
+        Node(package="simple_ros_cpp", executable="action_client", arguments=['5'])
         ])
