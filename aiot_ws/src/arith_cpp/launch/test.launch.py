bded7c074347bdeab2c03800d3482857b32e8071
freshmea <freshmea@naver.com>
2024-10-25 15:28:25 +0900

    calculator

--- aiot_ws/src/arith_cpp/launch/test.launch.py
+++ aiot_ws/src/arith_cpp/launch/test.launch.py
@@ -1,1 +1,9 @@
-#a
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(package="arith_cpp", executable="argument"),
+        Node(package="arith_cpp", executable="calculator")
+        ])
