2a760dae1caec417e7edb8ff7e2f5460b13de2ff
freshmea <freshmea@naver.com>
2024-10-29 11:15:06 +0900

    logging

--- aiot_ws/src/simple_ros_cpp/src/cyclic_pipline.cpp
+++ aiot_ws/src/simple_ros_cpp/src/cyclic_pipline.cpp
@@ -19,24 +19,30 @@ public:
         _sub = create_subscription<std_msgs::msg::Int32>(
             in,
             10,
-            [captured_pub](std_msgs::msg::Int32::UniquePtr msg)
+            [this, captured_pub](std_msgs::msg::Int32::UniquePtr msg)
             {
                 auto pub_ptr = captured_pub.lock();
                 if (!pub_ptr)
                 {
                     return;
                 }
-                printf("Receive msg value %d address 0x%" PRIXPTR "\n", msg->data,
-                       reinterpret_cast<std::uintptr_t>(msg.get()));
-                printf("sleeping for 1 second\n");
+                // printf("Receive msg value %d address 0x%" PRIXPTR "\n", msg->data,
+                //        reinterpret_cast<std::uintptr_t>(msg.get()));
+                // printf("sleeping for 1 second\n");
+                RCLCPP_INFO(get_logger(), "Receive msg value %d address 0x%" PRIXPTR, msg->data,
+                            reinterpret_cast<std::uintptr_t>(msg.get()));
+                RCLCPP_INFO(get_logger(), "sleeping for 1 second");
                 if (!rclcpp::sleep_for(1s))
                 {
                     return;
                 }
-                printf("done!");
+                // printf("done!\n");
+                RCLCPP_INFO(get_logger(), "done!");
                 msg->data++;
-                printf("Sending msg value %d address 0x%" PRIXPTR "\n", msg->data,
-                       reinterpret_cast<std::uintptr_t>(msg.get()));
+                // printf("Sending msg value %d address 0x%" PRIXPTR "\n", msg->data,
+                //        reinterpret_cast<std::uintptr_t>(msg.get()));
+                RCLCPP_INFO(get_logger(), "Sending msg value %d address 0x%" PRIXPTR, msg->data,
+                            reinterpret_cast<std::uintptr_t>(msg.get()));
                 pub_ptr->publish(std::move(msg));
             });
     }
@@ -50,7 +56,7 @@ int main(int argc, char *argv[])
     rclcpp::init(argc, argv);
     rclcpp::executors::SingleThreadedExecutor executor;
     auto pipe1 = std::make_shared<IncrementerPipe>("pipe1", "topic1", "topic2");
-    auto pipe2 = std::make_shared<IncrementerPipe>("pipe2", "topic1", "topic2");
+    auto pipe2 = std::make_shared<IncrementerPipe>("pipe2", "topic2", "topic1");
     rclcpp::sleep_for(1s);
     std::unique_ptr<std_msgs::msg::Int32> msg(new std_msgs::msg::Int32());
     msg->data = 42;
