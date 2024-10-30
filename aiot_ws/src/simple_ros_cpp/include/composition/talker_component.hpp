75ac879bace25e7cc50b240d77c8453e7b489d23
freshmea <freshmea@naver.com>
2024-10-30 11:59:36 +0900

    component

--- aiot_ws/src/simple_ros_cpp/include/composition/talker_component.hpp
+++ aiot_ws/src/simple_ros_cpp/include/composition/talker_component.hpp
@@ -11,11 +11,17 @@ namespace composition
     class Talker : public rclcpp::Node
     {
     public:
-        COMPOSITION_PUBLIC;
+        COMPOSITION_PUBLIC
+        explicit Talker(const rclcpp::NodeOptions &options);
+
+    protected:
+        void on_timer();
 
     private:
-    }
-
+        size_t _count;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub;
+        rclcpp::TimerBase::SharedPtr _timer;
+    };
 }
 
 #endif // COMPOSITION__TALKER_COMPONENT_HPP_
