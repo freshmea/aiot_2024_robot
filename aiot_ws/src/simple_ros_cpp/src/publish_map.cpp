e078032a5fbaa0c688dd69e3f0dcac5928b48d00
freshmea <freshmea@naver.com>
2024-11-07 14:37:15 +0900

    map

--- aiot_ws/src/simple_ros_cpp/src/publish_map.cpp
+++ aiot_ws/src/simple_ros_cpp/src/publish_map.cpp
@@ -1,39 +1,56 @@
+#include "nav_msgs/msg/occupancy_grid.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
 #include <chrono>
-#include <iostream>
 
 using namespace std;
 using namespace std::chrono_literals;
 
-class HellowPublisher : public rclcpp::Node
+class PublishMap : public rclcpp::Node
 {
 public:
-    explicit HellowPublisher()
-        : Node("hello"), _count(0)
+    explicit PublishMap()
+        : Node("publish_map"), _count(0)
     {
-        _pub = create_publisher<std_msgs::msg::String>("message", 10);
-        _timer = create_wall_timer(1s, std::bind(&HellowPublisher::printHello, this));
+        _pub = create_publisher<nav_msgs::msg::OccupancyGrid>("map", 10);
+        _timer = create_wall_timer(1s, std::bind(&PublishMap::pub_callback, this));
     }
 
 private:
     int _count;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr _pub;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr _pub;
     rclcpp::TimerBase::SharedPtr _timer;
-    void printHello()
+    void pub_callback()
     {
-        auto msg = std_msgs::msg::String();
-        msg.data = "Hello, World!!!!! " + to_string(_count);
+        auto msg = nav_msgs::msg::OccupancyGrid();
+        msg.header.frame_id = "world";
+        msg.header.stamp = get_clock()->now();
+
+        // map info
+        msg.info.resolution = 0.1f;
+        msg.info.width = 100;
+        msg.info.height = 100;
+        msg.info.origin.position.x = -(msg.info.width * msg.info.resolution) / 2;
+        msg.info.origin.position.y = -(msg.info.height * msg.info.resolution) / 2;
+        msg.info.origin.position.z = 0;
+        msg.info.origin.orientation.x = 0;
+        msg.info.origin.orientation.y = 0;
+        msg.info.origin.orientation.z = 0;
+        msg.info.origin.orientation.w = 1;
+
+        msg.data.resize(msg.info.width * msg.info.height);
+        for (auto i : msg.data)
+        {
+            i = -1;
+        }
+
         _pub->publish(msg);
-        RCLCPP_INFO(get_logger(), msg.data.c_str());
-        _count++;
     }
 };
 
 int main()
 {
     rclcpp::init(0, nullptr);
-    auto node = std::make_shared<HellowPublisher>();
+    auto node = std::make_shared<PublishMap>();
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
