99e9bcbbe6573650ce8a142b822ac4caac5feda6
freshmea <freshmea@naver.com>
2024-11-13 10:38:44 +0900

    servo_server

--- aiot_ws/src/move_turtle/move_turtle/servo_server.py
+++ aiot_ws/src/move_turtle/move_turtle/servo_server.py
@@ -3,36 +3,48 @@ import time
 import rclpy
 import RPi.GPIO as gpio
 from rclpy.node import Node
-from std_srvs.srv import 
+from tb_interface.srv import ServoAngle
+
+from aiot_ws.servo_test import set_servo_angle
 
 
-class LedServer(Node):
+class ServoServer(Node):
     def __init__(self):
-        super().__init__("service_server")
-        self.create_service(SetBool, "led_on_off", self.setBool_callback)
+        super().__init__("servo_server")
+        self.create_service(ServoAngle, "servo_angle", self.set_angle_callback)
         self.bool = bool()
+        self.servo_pin = 13
         gpio.setmode(gpio.BCM)
-        gpio.setup(21, gpio.OUT)
+        gpio.setup(self.servo_pin, gpio.OUT)
+        self.pwm = gpio.PWM(self.servo_pin, 50)
+        self.pwm.start(0)
+        self.theta = 0.0
+        self.pwm.ChangeDutyCycle(2.5)
+        time.sleep(1)
+        self.pwm.ChangeDutyCycle(0)
 
-    def setBool_callback(self, request : SetBool.Request, response : SetBool.Response):
-        self.get_logger().info(f"request {request.data}")
-        self.get_logger().info(f"internal bool {self.bool}")
-        if request.data != self.bool:
-            self.bool = not self.bool
-            if self.bool:
-                gpio.output(21, gpio.HIGH)
-            else:
-                gpio.output(21, gpio.LOW)
+    def set_servo_angle(self, angle, seconds):
+        duty_cycle = 2.5 + (angle/3.14*180/18.0)
+        self.pwm.ChangeDutyCycle(duty_cycle)
+        time.sleep(seconds)
+        self.pwm.ChangeDutyCycle(0)
+        self.theta = angle
+
+    def set_angle_callback(self, request : ServoAngle.Request, response : ServoAngle.Response):
+        ITER_NUM = 10
+        for i in range(ITER_NUM):
+            self.set_servo_angle(self.theta -(self.theta - request.theta)/ITER_NUM*(i+1), request.seconds/ITER_NUM)
+        if self.theta == request.theta:
+            response.delta_theta = self.theta - request.theta
             response.success = True
-            response.message = f"{self.bool} setting sucess"
         else:
+            response.delta_theta = self.theta - request.theta
             response.success = False
-            response.message = f"{self.bool} setting fail"
         return response
 
 def main():
     rclpy.init()
-    node = LedServer()
+    node = ServoServer()
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
