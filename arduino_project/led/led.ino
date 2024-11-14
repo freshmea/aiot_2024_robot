81de97143368e9c502c3275bbfb2b9864bf8feb2
freshmea <freshmea@naver.com>
2024-11-14 11:33:15 +0900

    aa

--- arduino_project/led/led.ino
+++ arduino_project/led/led.ino
@@ -1,12 +1,24 @@
+#include <Arduino.h>
+#include <string.h>
+
 void setup()
 {
     pinMode(13, OUTPUT);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-    digitalWrite(13, HIGH);
-    delay(1000);
-    digitalWrite(13, LOW);
-    delay(1000);
+    String buffer;
+    if (Serial.available() > 0)
+    {
+        buffer = Serial.readStringUntil('\n');
+        Serial.print("Echo : ");
+        Serial.println(buffer);
+    }
+    delay(100);
+    // digitalWrite(13, HIGH);
+    // delay(1000);
+    // digitalWrite(13, LOW);
+    // delay(1000);
 }
